c84ff2b9d55af100583ca4c815b06484fd7779f2
andrehatlo <ahatloj@gmail.com>
2021-01-19 14:07:06 +0100

    add chart time days weeks months

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import _ from 'lodash';
-
-const cc = require('cryptocompare')
-cc.setApiKey('4d0c36a5cf50aac54b6b42d3356e6bc199058cd9fbcf2e338ac9997edd8d3cc7')
+import moment from 'moment';
+const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
 
 const MAX_FAVORITES = 10;
+const TIME_UNITS = 10;
 
-export class AppProvider extends React.Component{
-    constructor(props) {
+export class AppProvider extends React.Component {
+    constructor(props){
         super(props);
         this.state = {
             page: 'dashboard',
-            favorites: ['BTC','ETH','XMR','DOGE'],
+            favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
+            timeInterval: 'months',
             ...this.savedSettings(),
             setPage: this.setPage,
             addCoin: this.addCoin,
@@ -21,76 +22,112 @@ export class AppProvider extends React.Component{
             isInFavorites: this.isInFavorites,
             confirmFavorites: this.confirmFavorites,
             setCurrentFavorite: this.setCurrentFavorite,
-            setFilteredCoins: this.setFilteredCoins
+            setFilteredCoins: this.setFilteredCoins,
+            changeChartSelect: this.changeChartSelect
         }
     }
 
     componentDidMount = () => {
-      this.fetchCoins();
-      this.fetchPrices();
+        this.fetchCoins();
+        this.fetchPrices();
+        this.fetchHistorical();
     }
 
-    fetchCoins = async() => {
+    fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
-        this.setState({coinList})
+        this.setState({coinList});
     }
 
-    fetchPrices = async() => {
+    fetchPrices = async () => {
         if(this.state.firstVisit) return;
         let prices = await this.prices();
-        // We must filter the empty price objects (not in the lecture)
         prices = prices.filter(price => Object.keys(price).length);
         this.setState({prices});
     }
 
-    prices = async() => {
-      let returnData = [];
-        // console.log(this.state.favorites);
-      for(let i = 0 ; i < this.state.favorites.length; i++){
-        try{
-          let priceData = await cc.priceFull(this.state.favorites[i], 'USD')
-          returnData.push(priceData);
-        } catch(e) {
-          console.warn('Fetch price error: ', e);
+    fetchHistorical = async () => {
+        if (this.state.firstVisit) return;
+        let results = await this.historical();
+        let historical = [
+            {
+                name: this.state.currentFavorite,
+                data: results.map((ticker, index) => [
+                    moment().subtract({[this.state.timeInterval]: TIME_UNITS - index}).valueOf(),
+                    ticker.USD
+                ])
+            }
+        ]
+        this.setState({historical});
+    }
+
+    prices = async () => {
+        let returnData = [];
+        for(let i = 0; i < this.state.favorites.length; i++){
+            try {
+                let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+                returnData.push(priceData);
+            } catch (e){
+                console.warn('Fetch price error: ', e);
+            }
         }
-      }
         return returnData;
     }
 
+    historical = () => {
+        let promises = [];
+        for (let units = TIME_UNITS; units > 0; units--){
+            promises.push(
+                cc.priceHistorical(
+                    this.state.currentFavorite,
+                    ['USD'],
+                    moment()
+                        .subtract({[this.state.timeInterval]: units})
+                        .toDate()
+                )
+            )
+        }
+        return Promise.all(promises);
+    }
+
     addCoin = key => {
-      let favorites = [...this.state.favorites];
-      if(favorites.length < MAX_FAVORITES) {
-        favorites.push(key);
-        this.setState({favorites});
-      }
+        let favorites = [...this.state.favorites];
+        if(favorites.length < MAX_FAVORITES){
+            favorites.push(key);
+            this.setState({favorites});
+        }
     }
 
     removeCoin = key => {
-      let favorites = [...this.state.favorites];
-      this.setState({favorites: _.pull(favorites,key)});
-     }
+        let favorites = [...this.state.favorites];
+        this.setState({favorites: _.pull(favorites, key)})
+    }
 
-     isInFavorites = key => _.includes(this.state.favorites, key);
+    isInFavorites = key => _.includes(this.state.favorites, key)
 
     confirmFavorites = () => {
         let currentFavorite = this.state.favorites[0];
-          this.setState({
+        this.setState({
             firstVisit: false,
             page: 'dashboard',
-              currentFavorite,
-          },() => {
+            currentFavorite,
+            prices: null,
+            historical: null
+        }, () => {
             this.fetchPrices();
-          });
-          localStorage.setItem('cryptoDash', JSON.stringify({
+            this.fetchHistorical();
+        });
+        localStorage.setItem('cryptoDash', JSON.stringify({
             favorites: this.state.favorites,
-              currentFavorite
-          }));
+            currentFavorite
+        }));
     }
 
     setCurrentFavorite = (sym) => {
         this.setState({
-            currentFavorite: sym
-        })
+            currentFavorite: sym,
+            historical: null
+        }, this.fetchHistorical);
+
         localStorage.setItem('cryptoDash', JSON.stringify({
             ...JSON.parse(localStorage.getItem('cryptoDash')),
             currentFavorite: sym
@@ -98,23 +135,28 @@ export class AppProvider extends React.Component{
     }
 
     savedSettings(){
-      let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
-      if(!cryptoDashData){
-        return {page: 'settings', firstVisit: true}
-      }
-      let {favorites, currentFavorite} = cryptoDashData;
-      return{favorites, currentFavorite};
+        let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+        if(!cryptoDashData){
+            return {page: 'settings', firstVisit: true}
+        }
+        let {favorites, currentFavorite} = cryptoDashData;
+        return {favorites, currentFavorite};
     }
 
     setPage = page => this.setState({page})
 
     setFilteredCoins = (filteredCoins) => this.setState({filteredCoins})
 
+    changeChartSelect = (value) => {
+        this.setState({timeInterval: value, historical: null}, this.fetchHistorical);
+    }
+
     render(){
-        return(
+        return (
             <AppContext.Provider value={this.state}>
                 {this.props.children}
             </AppContext.Provider>
         )
     }
+
 }
--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -4,14 +4,28 @@ import {Tile} from "../Shared/Tile";
 import {AppContext} from "../App/AppProvider";
 import ReactHighcharts from "react-highcharts";
 import HighchartsTheme from "./HighchartsTheme";
+import ChartSelect from "./ChartSelect";
+
+
 ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 export default function () {
     return(
         <AppContext.Consumer>
-            {({}) =>
+            {({historical, changeChartSelect}) =>
                 <Tile>
-                    <ReactHighcharts config={highchartsConfig()}/>
+                    <ChartSelect
+                        defaultValues={"months"}
+                        onChange={e => changeChartSelect(e.target.value)}
+                    >
+                        <option value = 'days'> Days </option>
+                        <option value = 'months'> Weeks </option>
+                        <option value = 'Weeks'> Months </option>
+                    </ChartSelect>
+                    {historical ?
+                        <ReactHighcharts config={highchartsConfig(historical)}/>
+                        : <div> LOADING HISTORICAL DATA </div>
+                    }
                 </Tile>
             }
         </AppContext.Consumer>
